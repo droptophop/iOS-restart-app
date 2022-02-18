ed6ba8f6fab0667e7ae123b9ec2bb18841998653
Daniel Hopson <hdhopson88@yahoo.com>
2022-02-18 11:38:05 -0600

    Adds audio and haptics features

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -19,6 +19,8 @@ struct OnboardingView: View {
     @State private var indicatorOpacity: Double = 1.0
     @State private var textTitle: String = "Share"
     
+    let hapticFeedback = UINotificationFeedbackGenerator()
+    
     // MARK: - BODY
     
     var body: some View {
@@ -160,9 +162,12 @@ struct OnboardingView: View {
                                 .onEnded { _ in
                                     withAnimation(Animation.easeOut(duration: 0.4)) {
                                         if buttonOffset > buttonWidth / 2 {
+                                            hapticFeedback.notificationOccurred(.success)
+                                            playSound(sound: "chimeup", type: "mp3")
                                             buttonOffset = buttonWidth - 80
                                             isOnboardingViewActive = false
                                         } else {
+                                            hapticFeedback.notificationOccurred(.warning)
                                             buttonOffset = 0
                                         }
                                     }
@@ -183,8 +188,11 @@ struct OnboardingView: View {
         .onAppear(perform: {
             isAnimating = true
         })
+        .preferredColorScheme(.dark)
     }
 }
+
+// MARK: - PREVIEW
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
